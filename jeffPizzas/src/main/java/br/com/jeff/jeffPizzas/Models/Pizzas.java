54956f4611623a50ba5f26928c030a8a71799005
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-05-06 17:58:30 -0300

    Feat: Pizza model adding into the model layer

---
+++ jeffPizzas/src/main/java/br/com/jeff/jeffPizzas/Models/Pizzas.java
@@ -0,0 +1,27 @@
+package br.com.jeff.jeffPizzas.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="T_PIZZAS_PIZZAS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Pizzas {
+    
+    @Id
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private double basePrice;
+}
