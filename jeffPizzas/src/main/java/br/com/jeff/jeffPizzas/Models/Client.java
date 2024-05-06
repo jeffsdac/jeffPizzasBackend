7a1be1c7445a4a2f2f347b641f27370385c9b5d4
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-05-06 17:07:52 -0300

    feat: adding Cliente model to the project

---
+++ jeffPizzas/src/main/java/br/com/jeff/jeffPizzas/Models/Client.java
@@ -0,0 +1,37 @@
+package br.com.jeff.jeffPizzas.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Client {
+    
+    @Id
+    private int id;
+
+    private String name;
+
+    private String address;
+
+    private String cellphone;
+
+    private String role;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    private String email;
+
+    private String senha;
+}
