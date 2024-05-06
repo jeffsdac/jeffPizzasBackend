20887b1ce4d6659c73db1288970c88f769ad9db4
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-05-06 17:04:23 -0300

    first commit

---
+++ jeffPizzas/src/main/java/br/com/jeff/jeffPizzas/JeffPizzasApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeff.jeffPizzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeffPizzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeffPizzasApplication.class, args);
+	}
+
+}
---
+++ jeffPizzas/src/test/java/br/com/jeff/jeffPizzas/JeffPizzasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeff.jeffPizzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeffPizzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
