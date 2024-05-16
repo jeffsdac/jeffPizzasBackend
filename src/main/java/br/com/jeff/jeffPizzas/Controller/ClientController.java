768678172997e9cf18ece0caf0ff706194e7b7ec
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-05-16 20:24:38 -0300

    Fixed: Adding cross origin

--- src/main/java/br/com/jeff/jeffPizzas/Controller/ClientController.java
+++ src/main/java/br/com/jeff/jeffPizzas/Controller/ClientController.java
@@ -3,6 +3,7 @@ package br.com.jeff.jeffPizzas.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import br.com.jeff.jeffPizzas.Repository.ClientRepository;
 
 @RestController
 @RequestMapping("/api/client")
+@CrossOrigin("*")
 public class ClientController {
     
     @Autowired
